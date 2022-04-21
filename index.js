5432091930ee13c2316854204d3d2b54f03c7b44
MateCon <mateoziffer@gmail.com>
2022-04-21 11:34:01 -0300

    setup node

--- index.js
+++ index.js
@@ -2,14 +2,6 @@ const express = require("express");
 const app = express();
 const PORT = 3000;
 
-const process_data = () => {
-	let x = 100;
-
-	return {
-		resultado: x,
-	};
-};
-
 app.use(express.json());
 
 app.get("/", function (req, res) {
