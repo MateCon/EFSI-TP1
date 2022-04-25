705f1a5febb32b492e0f267fe213e2d13f9c0569
MateCon <mateoziffer@gmail.com>
2022-04-25 08:50:06 -0300

    crear endpoints obtener_carton, cartones

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
-import express from "express";
+import express, { response } from "express";
+import { crear_cartones } from "./lib/bingo.js";
 import { random_number } from "./lib/math.js";
 const app = express();
 const PORT = 8080;
 
 app.use(express.json());
+
+let cartones = [];
+let asignaciones = [];
+let carton_actual = 0;
 
 app.get("/", (_, res) => {
 	res.send("Hello World!");
@@ -20,7 +25,57 @@ app.post("/numero_aleatorio", (req, res) => {
 	res.status(200).json({ resultado: random_number(1, req.body.valor) });
 });
 
+app.post("/iniciar_juego", (req, res) => {
+	const cantCartones = parseInt(req.body.value);
+
+	if (isNaN(cantCartones)) {
+		res.status(400).send("Valor incorrecto");
+		return;
+	}
+
+	cartones = crear_cartones(cantCartones);
+	asignaciones = [];
+	carton_actual = 0;
+	res.status(200).send("Juego iniciado");
+});
+
+app.get("/obtener_carton", (req, res) => {
+	const nombre = req.body.nombre;
+
+	if (!nombre) {
+		res.status(400).send("Nombre no encontrado");
+		return;
+	}
+
+	if (carton_actual >= cartones.length - 1) {
+		res.status(200).send("No mas cartones");
+		return;
+	}
+
+	asignaciones.push({
+		nombre,
+		index_carton: carton_actual,
+	});
+
+	carton_actual++;
+
+	res.status(200).send(cartones[carton_actual - 1]);
+});
+
+app.get("/cartones", (req, res) => {
+	res.status(200).json(cartones);
+});
+
+app.get("/cartones/:nombre", (req, res) => {
+	const nombre = req.params.nombre;
+
+	const asignaciones_de_persona = asignaciones.filter(
+		(asignacion) => asignacion.nombre === nombre
+	);
+
+	res.status(200).json(asignaciones_de_persona);
+});
+
 app.listen(PORT, () => {
-	console.log(random_number(1, 10));
 	console.log("Server listening on PORT", PORT);
 });
--- src/lib/bingo.js
+++ src/lib/bingo.js
@@ -1,27 +1,28 @@
-/*
-[
-    { "valores": [24, 53, 24, 32, ...], "owner": "Fausto" }
-    { "valores": [24, 53, 24, 32, ...], "owner": "Mateo" }
-    { "valores": [24, 53, 24, 32, ...], "owner": "Colo" }
-]
-*/
-
-import { random_number } from "./math";
+import { random_number } from "./math.js";
 
 const LENGTH_CARTON = 10;
 const MAX_NUMBER = 100;
 
 const crear_carton = () => {
-	// const valores = new Array(length)
-	// 	.fill(0)
-	// 	.map(() => random_number(1, length));
 	const valores = [];
 
 	for (let i = 0; i < LENGTH_CARTON; i++) {
-		valores.push(random_number(1, MAX_NUMBER));
+		let rnd = random_number(1, MAX_NUMBER);
+		while (!valores.includes(rnd)) {
+			rnd = random_number(1, MAX_NUMBER);
+		}
+		valores.push(rnd);
 	}
 
 	return valores;
 };
 
-export const crear_cartones = () => {};
+export const crear_cartones = (cant) => {
+	const resultado = [];
+
+	for (let i = 0; i < cant; i++) {
+		resultado.push(crear_carton());
+	}
+
+	return resultado;
+};
