5bc11cda2904d0ca8196962d4b11ba282cbf8c28
MateCon <68882595+MateCon@users.noreply.github.com>
2022-04-25 18:42:59 -0300

    terminar endpoints

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
-import express, { response } from "express";
-import { crear_cartones } from "./lib/bingo.js";
+import express from "express";
+import {
+	crear_cartones,
+	crear_numeros_posibles,
+	elegir_numero,
+} from "./lib/bingo.js";
 import { random_number } from "./lib/math.js";
 const app = express();
 const PORT = 8080;
@@ -8,7 +12,19 @@ app.use(express.json());
 
 let cartones = [];
 let asignaciones = [];
-let carton_actual = 0;
+let carton_actual; // para asignar los cartones antes de comenzar el juego
+let numeros_posibles; // para ir sacando numeros al azar durante el juego
+let juego_terminado;
+/*
+Esta es la forma que utilizamos para guardar
+los cartones y la forma de asignarle un
+carton a un jugador:
+
+Carton       : { numero, fueMarcado }[],
+Cartones     : Carton[]
+Asignacion   : { nombre, indexCarton }
+Asignaciones : Asignacion[]
+*/
 
 app.get("/", (_, res) => {
 	res.send("Hello World!");
@@ -36,6 +52,8 @@ app.post("/iniciar_juego", (req, res) => {
 	cartones = crear_cartones(cantCartones);
 	asignaciones = [];
 	carton_actual = 0;
+	numeros_posibles = crear_numeros_posibles();
+	juego_terminado = false;
 	res.status(200).send("Juego iniciado");
 });
 
@@ -54,7 +72,7 @@ app.get("/obtener_carton", (req, res) => {
 
 	asignaciones.push({
 		nombre,
-		index_carton: carton_actual,
+		indexCarton: carton_actual,
 	});
 
 	carton_actual++;
@@ -69,11 +87,48 @@ app.get("/cartones", (req, res) => {
 app.get("/cartones/:nombre", (req, res) => {
 	const nombre = req.params.nombre;
 
-	const asignaciones_de_persona = asignaciones.filter(
-		(asignacion) => asignacion.nombre === nombre
-	);
+	const cartones_de_persona = asignaciones
+		// filtrar las asignaciones que no son del usuario
+		.filter((asignacion) => asignacion.nombre === nombre)
+		// convertir las asignaciones a los cartones que apuntan
+		.map((asignacion) => cartones[asignacion.indexCarton]);
 
-	res.status(200).json(asignaciones_de_persona);
+	res.status(200).json(cartones_de_persona);
+});
+
+/*
+Posible mejora: guardar dentro del carton un contador de
+cuantos numeros se marcaron para evitar el every que
+se usa dentro del primer
+*/
+app.get("/sacar_numero", (req, res) => {
+	if (juego_terminado) {
+		res.status(200).send("el juego ya ha terminado");
+		return;
+	}
+	const data = elegir_numero(numeros_posibles);
+	numeros_posibles = data.numeros_posibles;
+
+	for (let i = 0; i < cartones.length; i++) {
+		cartones[i].forEach((cuadrado) => {
+			if (cuadrado.numero === data.resultado) {
+				cuadrado.fueMarcado = true;
+			}
+		});
+
+		if (cartones[i].every((cuadrado) => cuadrado.fueMarcado)) {
+			// persona ha ganado
+			const persona =
+				asignaciones.filter(
+					(asignacion) => asignacion.indexCarton === i
+				)[0]?.nombre ?? "Nadie";
+			juego_terminado = true;
+			res.status(200).send(`${persona} ha ganado!`);
+			return;
+		}
+	}
+
+	res.status(200).send(`Numero eliminado: ${data.resultado}`);
 });
 
 app.listen(PORT, () => {
--- src/lib/bingo.js
+++ src/lib/bingo.js
@@ -1,17 +1,20 @@
 import { random_number } from "./math.js";
 
-const LENGTH_CARTON = 10;
-const MAX_NUMBER = 100;
+const LENGTH_CARTON = 25;
+const MAX_NUMBER = 75;
 
 const crear_carton = () => {
 	const valores = [];
 
 	for (let i = 0; i < LENGTH_CARTON; i++) {
 		let rnd = random_number(1, MAX_NUMBER);
-		while (!valores.includes(rnd)) {
+		while (valores.includes(rnd)) {
 			rnd = random_number(1, MAX_NUMBER);
 		}
-		valores.push(rnd);
+		valores.push({
+			numero: rnd,
+			fueMarcado: false,
+		});
 	}
 
 	return valores;
@@ -25,4 +28,27 @@ export const crear_cartones = (cant) => {
 	}
 
 	return resultado;
+};
+
+export const crear_numeros_posibles = () => {
+	const resultado = [];
+
+	for (let i = 0; i < MAX_NUMBER; i++) {
+		resultado.push(i);
+	}
+
+	return resultado;
+};
+
+export const elegir_numero = (numeros_posibles) => {
+	const idx = random_number(0, numeros_posibles.length - 1);
+	const resultado = numeros_posibles[idx];
+	return {
+		resultado,
+		// devolver los numeros posibles sin el resultado elegido
+		numeros_posibles: [
+			...numeros_posibles.slice(0, idx),
+			...numeros_posibles.slice(idx + 1),
+		],
+	};
 };
