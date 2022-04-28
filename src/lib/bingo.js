c8bfc8a86e9862dd66cc4811add098165ff4562c
MateCon <mateoziffer@gmail.com>
2022-04-28 11:58:42 -0300

    .

--- src/lib/bingo.js
+++ src/lib/bingo.js
@@ -1,20 +1,10 @@
-import { random_number } from "./math.js";
+import { random_array, random_number } from "./math.js";
 
 const LENGTH_CARTON = 25;
 const MAX_NUMBER = 75;
 
 const crear_carton = () => {
-	const valores = [];
-
-	for (let i = 0; i < LENGTH_CARTON; i++) {
-		let rnd = random_number(1, MAX_NUMBER);
-		while (valores.includes(rnd)) {
-			rnd = random_number(1, MAX_NUMBER);
-		}
-		valores.push(rnd);
-	}
-
-	return valores.map(numero => ({
+	return random_array(LENGTH_CARTON, 1, MAX_NUMBER).map(numero => ({
 		numero,
 		fueMarcado: false
 	}));
@@ -52,3 +42,5 @@ export const elegir_numero = (numeros_posibles) => {
 		],
 	};
 };
+
+console.log(crear_carton());
--- src/lib/math.js
+++ src/lib/math.js
@@ -1,3 +1,17 @@
 export const random_number = (min, max) => {
 	return Math.floor(Math.random() * (max - min)) + min;
 };
+
+export const random_array = (length, min, max) => {
+	const valores = [];
+
+	for (let i = 0; i < length; i++) {
+		let rnd = random_number(min, max);
+		while (valores.includes(rnd)) {
+			rnd = random_number(min, max);
+		}
+		valores.push(rnd);
+	}
+
+	return valores;
+};
