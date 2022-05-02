2847004f05e2a12006eaec1d8371cf96ad0bb247
MateCon <mateoziffer@gmail.com>
2022-05-02 10:36:04 -0300

    pseudo_random_arr

--- src/lib/bingo.js
+++ src/lib/bingo.js
@@ -1,12 +1,33 @@
 import { random_array, random_number } from "./math.js";
 
-const LENGTH_CARTON = 25;
-const MAX_NUMBER = 75;
+const LENGTH_CARTON = 15;
+const MAX_NUMBER = 100;
+
+const pseudo_random_array = () => {
+	const arr = [];
+	const decenas = [
+		...[0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+		...random_array(5, 0, 9),
+	];
+
+	for (let i = 0; i < decenas.length; i++) {
+		const min = decenas[i] * 10;
+		const max = decenas[i] * 10 + 9;
+
+		if (i === 0) arr.push(random_number(min + 1, max));
+		else if (i === 90) arr.push(random_number(min, max + 1));
+		else arr.push(random_number(min, max));
+	}
+
+	return arr.sort(() => Math.random() - 0.5);
+};
+
+console.log(pseudo_random_array());
 
 const crear_carton = () => {
-	return random_array(LENGTH_CARTON, 1, MAX_NUMBER).map(numero => ({
+	return pseudo_random_array().map((numero) => ({
 		numero,
-		fueMarcado: false
+		fueMarcado: false,
 	}));
 };
 
@@ -42,5 +63,3 @@ export const elegir_numero = (numeros_posibles) => {
 		],
 	};
 };
-
-console.log(crear_carton());
